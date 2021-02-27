1c1353616e4f92bfcabb7be1b7577e8aa1970a2f
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2021-02-27 17:05:45 +0530

    traits presentation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use rayon::prelude::*;
-use std::time::Instant;
 use std::{thread, time};
+use std::string::String;
+use rayon::prelude::*;
 
 
 /// Generate accuracy for ytest and ypreds
@@ -43,6 +43,32 @@ fn sum_of_squares_rayon() {
     println!("sum_of_squares_normal -> {}", res);
 }
 
+struct Tennis {
+    tennis_ball: String,
+}
+
+struct Cricket {
+    red_ball: String,
+}
+
+trait Play {
+    fn play(&self, player: &str);
+}
+
+impl Play for Tennis {
+    fn play(&self, player: &str) {
+        println!("{player} plays in US open with {ball}",
+            player=player.to_string(), ball=self.tennis_ball);
+    }
+}
+
+impl Play for Cricket {
+    fn play(&self, player: &str) {
+        println!("{player} plays in IPL with {ball}",
+            player=player.to_string(), ball=self.red_ball);
+    }
+}
+
 
 fn main() {
     // using functions
@@ -51,24 +77,43 @@ fn main() {
     // let acc = accuracy(&xs, &ys);
     // println!("accuracy {}", acc);
 
-    // rust concurrency using libraries
-    // we will also benchmark the result.
-    // for benchmarking though the recommended one is to use
-    // the fdefault benchmarking tool
-    // https://stackoverflow.com/a/57341631/5417164
-    let now = Instant::now();
-    {
-        sum_of_squares_normal();
-    }
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:#?}", elapsed);
+    // // rust concurrency using libraries
+    // // we will also benchmark the result.
+    // // for benchmarking though the recommended one is to use
+    // // the fdefault benchmarking tool
+    // // https://stackoverflow.com/a/57341631/5417164
+    // let now = Instant::now();
+    // {
+    //     sum_of_squares_normal();
+    // }
+    // let elapsed = now.elapsed();
+    // println!("Elapsed: {:#?}", elapsed);
 
-    let now = Instant::now();
-    {
-        sum_of_squares_rayon();
-    }
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:#?}", elapsed);
+    // let now = Instant::now();
+    // {
+    //     sum_of_squares_rayon();
+    // }
+    // let elapsed = now.elapsed();
+    // println!("Elapsed: {:#?}", elapsed);
+
+    // // normal concurrency
+    // let handle = thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("hi number {} from the spawned thread!", i);
+    //         thread::sleep(time::Duration::new(1, 0));
+    //     }
+    // });
+    // for i in 1..5 {
+    //     println!("hi number {} from the main thread!", i);
+    //     thread::sleep(time::Duration::new(1, 0));
+    // }
+    // handle.join().unwrap();
+
+    // traits and structs
+    let tennis = Tennis{tennis_ball: "tennis ball".to_string()};
+    tennis.play("player");
+    let cricket = Cricket{red_ball: "red ball".to_string()};
+    cricket.play("player");
 }
 
 
